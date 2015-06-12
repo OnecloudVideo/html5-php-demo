55aa3c7658ba8c40a8be185cf679449bc5f96b31
keijack <keijack.wu@gmail.com>
2015-06-12 16:25:32 +0800

    1、更友好的进度条 2、修改了算法，可以清除服务器错误的分片。

--- static/js/restful-video-upload.js
+++ static/js/restful-video-upload.js
@@ -11,6 +11,7 @@
  * accordance with the terms of the license agreement you entered into
  * with OneCloud.
  */
+
 var VideoUpload = (function($) {
 	'use strict';
 
@@ -53,9 +54,9 @@ var VideoUpload = (function($) {
 		accept : [ '.avi', '.flv', '.mp4', '.mpeg', '.mpg', '.wmv', '.mkv', '.mts', '.wav', '.flac', '.ape', '.mp3',
 				'.wma', '.m4a', '.aac' ],
 
-	    /*
-	     * 设置上传文件的分类id, 可选, 没有指定则为'默认'分类。可以利用亦云视频关于分类的restful api查询分类的id。
-	     * 若在初始化时不确定，可以在uploader中调用setCatalogId()设置。
+		/*
+		 * 设置上传文件的分类id, 可选, 没有指定则为'默认'分类。可以利用亦云视频关于分类的restful api查询分类的id。
+		 * 若在初始化时不确定，可以在uploader中调用setCatalogId()设置。
 		 */
 		catalogId : '',
 
@@ -75,7 +76,11 @@ var VideoUpload = (function($) {
 		onQueueComplete : function() {
 		},
 
-		// 文件开始上传时触发
+		// 文件开始扫描时触发
+		onStart : function(fileItem) {
+		},
+
+		// 文件扫描完，正式开始上传时触发
 		onUploadStart : function(fileItem) {
 		},
 
@@ -105,11 +110,14 @@ var VideoUpload = (function($) {
 
 		// 上传完成, 通知亦云视频合并文件
 		completeUrl : videoApiDomain + "/video/multipartUpload/complete.api",
-		
+
+		// 删除错误文件分片
+		deletePartsUrl : videoApiDomain + '/video/multipartUpload/deleteParts.api',
+
 		// 查询分类
 		getClassifyUrl : videoApiDomain + "/catalog/list.api",
 
-		// 上传文件的引用数组, 内部变量
+		// 上传文件的引用数组, 内部变量,
 		fileItemQueue : []
 	};
 
@@ -142,25 +150,39 @@ var VideoUpload = (function($) {
 			$fileElement.unbind("change").change(function() {
 				var $this = $(this);
 				var $fileInput = $this[0];
+				if ($fileInput.files.length > 5) {
+					alert("一次只能上传5个文件，请减少上传的文件。");
+					$this.val("");
+					return false;
+				}
+
+				var hasError = false, msg = "";
 				for (var fileIndex = 0, file; file = $fileInput.files[fileIndex++];) {
 					if (file.size > settings.limitSize) {
-						handlers.onSelectError(file.name, 'FILE_TOO_BIG');
-						return false;
+						msg += handlers.onSelectError(file.name, 'FILE_TOO_BIG');
+						hasError = true;
 					}
 					if (file.size == 0) {
-						handlers.onSelectError(file.name, 'FILE_ZERO');
-						return false;
+						msg += handlers.onSelectError(file.name, 'FILE_ZERO');
+						hasError = true;
 					}
 					var matches = file.name.toLowerCase().match(/(\.[^.]+)$/);
 					var ext = matches && matches[1] || "";
 					if ($.inArray(ext, settings.accept) < 0) {
-						handlers.onSelectError(file.name, 'FORMAT_NOT_SUPPORT');
-						return false;
+						msg += handlers.onSelectError(file.name, 'FORMAT_NOT_SUPPORT');
+						hasError = true;
 					}
 				}
+				if (hasError) {
+					alert(msg);
+					$this.val("");
+					return false;
+				}
+
 				if ($fileInput.files.length > 0) {
 					handlers.onSelect($fileInput);
 				}
+				$this.val("");
 			});
 
 			// 判断浏览器是否支持html5的File api
@@ -175,9 +197,8 @@ var VideoUpload = (function($) {
 	}; // end of Uploader
 
 	var HTML5Uploader = {
-		// 开始上传
 		upload : function() {
-			handlers.onUploadStart();
+			handlers.onStart();
 		},
 		// 设置上传文件的分类id, 可选, 没有指定则为'默认'分类。
 		setCatalogId : function(catalogId) {
@@ -192,7 +213,7 @@ var VideoUpload = (function($) {
 				param.sign = sdata.sign;
 				$.get(settings.getClassifyUrl, param, function(data) {
 					if (data.statusCode != 0) {
-						console.log("get classify fail");
+						// console.log("get classify fail");
 						fail();
 					} else {
 						success(data);
@@ -257,7 +278,7 @@ var VideoUpload = (function($) {
 		onUploadComplete : function(fileItem) {
 			settings.onUploadComplete(fileItem);
 			fileItem = null;
-			this.onUploadStart();
+			this.onStart();
 		},
 		/**
 		 * 当所有文件上传完成后, 清空文件列表
@@ -267,16 +288,22 @@ var VideoUpload = (function($) {
 			settings.onQueueComplete();
 		},
 		/**
-		 * 选择队列中的第一个开始上传
+		 * 单个文件开始上传
 		 */
-		onUploadStart : function() {
+		onStart : function() {
 			if (settings.fileItemQueue.length > 0) {
 				var fileItem = settings.fileItemQueue.shift();
+				settings.onStart(fileItem);
 				md5sum(fileItem, uploadFile);
-				settings.onUploadStart(fileItem);
 			} else {
 				this.onQueueComplete();
 			}
+		},
+		/**
+		 * 单个文件扫描完，正式开始上传
+		 */
+		onUploadStart : function(fileItem) {
+			settings.onUploadStart(fileItem);
 		},
 		/**
 		 * 上传文件时出错, 删除文件上传信息, 显示上传出错
@@ -297,17 +324,19 @@ var VideoUpload = (function($) {
 		 * 处理选择文件出错的情况
 		 */
 		onSelectError : function(fileName, errorCode) {
+			var msg = "";
 			switch (errorCode) {
 			case 'FILE_TOO_BIG':
-				alert("文件：" + fileName + "超过200MB，请使用FTP上传。");
+				msg = "文件：" + fileName + "超过200MB，请使用FTP上传。";
 				break;
 			case 'FILE_ZERO':
-				alert("文件：" + fileName + "大小为0，请上传正确的文件。");
+				msg = "文件：" + fileName + "大小为0，请上传正确的文件。";
 				break;
 			case 'FORMAT_NOT_SUPPORT':
-				alert("请选择以下格式的文件：\n" + settings.accept.toString());
+				msg = "请选择以下格式的文件：\n" + settings.accept.toString();
 				break;
 			}
+			return msg;
 		}
 	}; // end of handlers
 
@@ -323,7 +352,7 @@ var VideoUpload = (function($) {
 	 */
 	function md5sum(fileItem, success) {
 		var file = fileItem.nativeFile;
-		console.log(file.name + ", " + file.size);
+		// console.log(file.name + ", " + file.size);
 		var spark = new SparkMD5.ArrayBuffer();
 		var fileReader = new FileReader();
 
@@ -340,10 +369,11 @@ var VideoUpload = (function($) {
 				readNext();
 			} else {
 				var md5String = spark.end();
-				console.log("md5 : " + md5String);
+				// console.log("md5 : " + md5String);
 				spark.destroy();
 				fileReader = null;
 				$fileItem = null;
+				handlers.onUploadStart(fileItem);
 				success(fileItem, md5String);
 			}
 		}
@@ -361,12 +391,13 @@ var VideoUpload = (function($) {
 	 * 计算参数签名sign函数, 成功后调callback
 	 */
 	function getSign(formData, callback) {
-		$.post(settings.signUrl, formData, function(data, status, jqXHR) {
+		$.post(settings.signUrl, formData, function(data, status) {
 			if (status == "success") {
 				callback(data);
 			} else {
-				console.log("get sign fail")
-				console.log(jqXHR);
+				// console.log("get sign fail")
+				// console.log(jqXHR);
+				throw new Error("can't get sign");
 			}
 		});
 	}
@@ -423,28 +454,25 @@ var VideoUpload = (function($) {
 		 * 断点续传之前, 查询这个文件是否已经初始化。若是, 则进一步查询文件上传的进度进行续传; 否则, 进行初始化上传
 		 */
 		function startUpload() {
-			console.log(chunkArray);
-
+			// console.log(chunkArray);
 			getSign({
-				fileNameLike : fileItem.fileName,
 				fileMD5Equal : md5String
 			}, function(edata) {
-				console.log(edata);
+				// console.log(edata);
 				$.get(settings.listUrl, {
-					fileNameLike : fileItem.fileName,
 					fileMD5Equal : md5String,
 					accessKey : edata.accessKey,
 					time : edata.time,
 					sign : edata.sign
 				}, function(data) {
-					console.log(data);
+					// console.log(data);
 					if (data.multipartUploads.length == 0) {
 						initUpload();
-					} else if (data.multipartUploads.length > 1) {
-						console.log("continue upload same files");
-					} else {
+					} else if (data.multipartUploads.length == 1) {
 						fileItem.uploadId = data.multipartUploads[0].uploadId;
 						continueUpload();
+					} else {
+						// console.log("continue upload same files");
 					}
 				});
 			});
@@ -476,6 +504,28 @@ var VideoUpload = (function($) {
 		}
 
 		/**
+		 * XXX should upload in queue
+		 */
+		function updateTransferProgress() {
+			var obj = {};
+			var uploadedBytes = 0;
+			obj.updateProgress = function(e) {
+				if (e.lengthComputable) {
+					var percentage = ((uploadedBytes + e.loaded) / file.size) * 100;
+					handlers.onProgress(fileItem, percentage);
+				}
+			};
+			obj.addUploaded = function(uploaded) {
+				uploadedBytes += uploaded;
+			}
+			return obj;
+		}
+
+		var progressUpdater = updateTransferProgress();
+
+		var serverInfo = [];
+
+		/**
 		 * 计算上次断开的分块进行续传
 		 */
 		function continueUpload() {
@@ -493,13 +543,32 @@ var VideoUpload = (function($) {
 						return false;
 					}
 					// 从亦云视频查询上传完成的分块，与本地文件分块比较，找到断开的那个分块的序号
-					var serverInfo = data.uploadedParts;
+					serverInfo = data.uploadedParts;
+					// console.log("uploaded parts..." + serverInfo);
+					if (!serverInfo.length) {
+						uploadChunk();
+						return;
+					}
+					// 记录服务器上错误的 partKey，有可能是因为上传到一半，有可能是因为改变了大小导致不一致。
+					var errorParts = $.map(serverInfo, function(ele) {
+						return ele.partKey;
+					});
+
+					// console.log(serverInfo);
 					var parts = [];
 					for (var k = 0, clen = chunkArray.length; k < clen; k++) {
 						for (var j = 0, slen = serverInfo.length; j < slen; j++) {
-							if (serverInfo[j].partMD5 == chunkArray[k].md5) {
+							var serverChunk = serverInfo[j];
+							if (serverChunk.partMD5 == chunkArray[k].md5
+							// 服务器 1 base, 当前算法 0 base
+							&& serverChunk.partNumber == (chunkArray[k].partNum + 1)) {
 								chunkArray[k].partKey = serverInfo[j].partKey;
 								uploadInfoStorage.push(chunkArray[k]);
+								// 如果服务器上的正确，从错误里删除
+								errorParts = $.map(errorParts, function(ele) {
+									if (ele != serverChunk.partKey)
+										return ele;
+								});
 								break;
 							}
 							if (j == slen - 1) {
@@ -507,12 +576,42 @@ var VideoUpload = (function($) {
 							}
 						}
 					}
-					console.log(parts);
-					chunkArray = parts;
-					if (chunkArray.length > 0) {
-						uploadChunk();
+
+					// 删除错误的块
+					if (errorParts.length) {
+						console.log("erro parts..." + errorParts);
+						var partKeys = errorParts.toString();
+						getSign({
+							partKeys : partKeys
+						}, function(edata) {
+							$.post(settings.deletePartsUrl, {
+								partKeys : partKeys,
+								accessKey : edata.accessKey,
+								time : edata.time,
+								sign : edata.sign
+							}, function(data) {
+								if (data.statusCode != 0) {
+									handlers.onUploadError(fileItem);
+									return false;
+								}
+
+								chunkArray = parts;
+								if (chunkArray.length) {
+									progressUpdater.addUploaded(uploadInfoStorage.length * settings.chunkSize);
+									uploadChunk();
+								} else {
+									uploadComplete();
+								}
+							});
+						});
 					} else {
-						uploadComplete();
+						chunkArray = parts;
+						if (chunkArray.length) {
+							progressUpdater.addUploaded(uploadInfoStorage.length * settings.chunkSize);
+							uploadChunk();
+						} else {
+							uploadComplete();
+						}
 					}
 				});
 			});
@@ -527,7 +626,7 @@ var VideoUpload = (function($) {
 			var chunkInfo = chunkArray.shift();
 			var currentChunk = chunkInfo.partNum;
 
-			console.log("upload chunk : " + currentChunk);
+			// console.log("upload chunk : " + currentChunk);
 			var formData = new FormData();
 			formData.append("uploadId", fileItem.uploadId);
 			formData.append("partNumber", (currentChunk + 1)); // 亦云视频的分块序号是从1开始，而这里是从0开始，因此要+1
@@ -549,6 +648,11 @@ var VideoUpload = (function($) {
 					data : formData,
 					processData : false,
 					contentType : false,
+					xhr : function() {
+						var xhr = new window.XMLHttpRequest();
+						xhr.upload.addEventListener('progress', progressUpdater.updateProgress, false);
+						return xhr;
+					},
 					success : function(data, status, jqXHR) {
 						if (data.statusCode != 0) {
 							handlers.onUploadError(fileItem);
@@ -560,15 +664,13 @@ var VideoUpload = (function($) {
 						} else {
 							chunkInfo.partKey = data.partKey;
 							uploadInfoStorage.push(chunkInfo);
-
-							var percentage = Math.round(((currentChunk + 1) / chunks) * 100);
-							handlers.onProgress(fileItem, percentage);
+							progressUpdater.addUploaded(settings.chunkSize);
 						}
 						// 若队列还有分块，则继续上传
 						if (chunkArray.length > 0) {
 							uploadChunk();
 						} else {
-							console.log(uploadInfoStorage);
+							// console.log(uploadInfoStorage);
 							uploadComplete();
 						}
 					}
@@ -596,7 +698,7 @@ var VideoUpload = (function($) {
 				completeParas.time = edata.time;
 				completeParas.sign = edata.sign;
 				$.post(settings.completeUrl, completeParas, function(data) {
-					console.log(data);
+					// console.log(data);
 					if (data.statusCode != 0) {
 						handlers.onUploadError(fileItem);
 						return false;
--- static/js/upload.js
+++ static/js/upload.js
@@ -13,71 +13,78 @@
  */
 
 (function($) {
-
-	var uploadFileNumber = 0;
 	
+	var uploadFileNumber = 0;
 	/**
 	 * 初始化VideoUpload
 	 */
-	var uploader = VideoUpload
-			.init({
-				// 计算签名sign的url,
-				signUrl : '/html5/get_sign.php',
-
-				// 浏览文件的按钮id, the <input type='file'> element, #uploadFile
-				fileElement : '#uploadFile',
-
-				// 上传文件列表的id, #fileQueue
-				queueId : '#fileQueue',
-
-				// 浏览文件按钮名称
-				buttonText : '选择上传文件',
-
-				// 浏览文件按钮样式
-				buttonClass : 'but_add',
-
-				// 文件UI模板, $fileItemId, $fileName, $fileSize是文件属性变量
-				template : "<tr id='$fileItemId'>\
+	var uploader = VideoUpload.init({
+		// 计算签名sign的url, 请参考demo的webcontent/WEB-INF/web.xml文件中关于SignServlet的配置
+		signUrl : '/html5/sign',
+		
+		// 浏览文件的按钮id, the <input type='file'> element, #uploadFile
+		fileElement : '#uploadFile',
+		
+		// 上传文件列表的id, #fileQueue
+		queueId : '#fileQueue',
+		
+		// 浏览文件按钮名称
+		buttonText : '选择上传文件',
+		
+		// 浏览文件按钮样式
+		buttonClass : 'but_add',
+		
+		// 文件UI模板, $fileItemId, $fileName, $fileSize是文件属性变量
+		template : "<tr id='$fileItemId'>\
 					<td>$fileName<div class='pispower_upload_progress'>\
 					    <div class='pispower_upload_progress_bar'></div></div></td>\
 					<td width='25%'>$fileSize</td>\
 					<td class='status' width='25%'>等待</td></tr>",
-
-				/**
-				 * 上传开始, 更新状态为扫描
-				 */
-				onUploadStart : function(fileItem) {
-					$('#' + fileItem.fileItemId).find(".status").text("扫描");
-				},
-
-				/**
-				 * 更新进度。实时更新上传文件进度，更新状态为上传中。这里使用默认提供的上传进度条样式。
-				 */
-				onProgress : function(fileItem, percentage) {
-					var $fileItem = $('#' + fileItem.fileItemId);
-					$fileItem.find(".pispower_upload_progress_bar").css('width', percentage + '%');
-					$fileItem.find(".status").text('上传中');
-				},
-
-				/**
-				 * 上传成功，更新状态为完成。
-				 */
-				onUploadComplete : function(fileItem) {
-					$('#' + fileItem.fileItemId).find(".status").text('完成');
-					$("#uploadFileNumber").text(++uploadFileNumber);
-				},
-
-				/**
-				 * 上传失败，更新状态为上传失败。
-				 */
-				onUploadError : function(fileItem) {
-					$('#' + fileItem.fileItemId).find(".status").text('上传失败');
-				}
-			});
+		
+		// 上传文件的分类id, 可选, 没有指定则为'默认'分类。可以利用亦云视频关于分类的restful api查询分类的id
+		//catalogId : '',
+		
+		/**
+		 * 上传开始, 更新状态为扫描
+		 */			
+		onStart: function(fileItem) {
+			$('#' + fileItem.fileItemId).find(".status").text("扫描");
+		},
+		
+		/**
+		 * 扫描完成，开始正式上传
+		 */
+		onUploadStart : function(fileItem) {
+			$('#' + fileItem.fileItemId).find(".status").text('上传中');
+		},
+		
+		/**
+		 * 更新进度。实时更新上传文件进度，更新状态为上传中。这里使用默认提供的上传进度条样式。
+		 */
+		onProgress : function(fileItem, percentage) {
+			$('#' + fileItem.fileItemId).find(".pispower_upload_progress_bar").css('width', percentage + '%');
+		},
+		
+		/**
+		 * 上传成功，更新状态为完成。
+		 */
+		onUploadComplete : function(fileItem) {
+			$('#' + fileItem.fileItemId).find(".status").text('完成');
+			$("#uploadFileNumber").text(++uploadFileNumber);
+		},
+		
+		/**
+		 * 上传失败，更新状态为上传失败。
+		 */
+		onUploadError : function(fileItem) {
+			$('#' + fileItem.fileItemId).find(".status").text('上传失败');
+		}
+	});
+	
 	/**
 	 * 查询用户的分类
 	 */
-	uploader.getClassify(function(data){
+	uploader.getClassify(function(data) {
 		var catalogs = data.catalogs;
 		var $select = $("select[name=catalogId]");
 		for (var index = 0, cata; cata = catalogs[index++];) {
