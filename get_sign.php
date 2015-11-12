9f3629fdd1e7c665f34a3fe859a500584648ecdc
OnecloudVideo <OnecloudVideo@users.noreply.github.com>
2015-11-12 11:43:32 +0800

    Update get_sign.php

--- get_sign.php
+++ get_sign.php
@@ -5,9 +5,9 @@
 $params = $_REQUEST;
 
 /*
- * 按照亦云视频平台的需求，增加必要的参数。
+ * 按照鸿瑞云视频平台的需求，增加必要的参数。
  */
-$time = time () * 1000; // 亦云视频平台需要的是毫秒，time()函数返回的是秒
+$time = time () * 1000; // 鸿瑞云视频平台需要的是毫秒，time()函数返回的是秒
 $params ["time"] = $time;
 $params ["accessKey"] = OC_ACCESS_KEY;
 
