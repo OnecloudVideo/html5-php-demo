0c0576c35b1cc58abcba30779bcec420dddedcc2
OnecloudVideo <OnecloudVideo@users.noreply.github.com>
2015-11-12 11:47:22 +0800

    Update cal_sign.php

--- utils/cal_sign.php
+++ utils/cal_sign.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * 亦云视频云平台用来确定账户的参数，请访问 http://video.pispower.com 获取
+ * 鸿瑞云视频云平台用来确定账户的参数，请访问 http://video.pispower.com 获取
  *
  * @var string 
  */
 define ( 'OC_ACCESS_KEY', '' );
 /**
- * 亦云视频签名算法的一个私钥，请确保其私密性，不要泄露。请访问 http://video.pispower.com 获取
+ * 鸿瑞云视频签名算法的一个私钥，请确保其私密性，不要泄露。请访问 http://video.cloudak47.com 获取
  *
  * @var string
  */
 define ( 'OC_ACCESS_SECRET', '' );
 
 /**
- * 计算亦云视频云平台所需要的额外的签名参数
+ * 计算鸿瑞云视频云平台所需要的额外的签名参数
  *
  * @param array $params        	
  * @return string
